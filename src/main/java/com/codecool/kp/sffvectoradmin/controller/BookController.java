797ca26f124fffdcf5036370c053165893a12b98
Panna Kristóf <kristofpanna@gmail.com>
2020-03-09 19:03:33 +0100

    Add swagger

--- src/main/java/com/codecool/kp/sffvectoradmin/controller/BookController.java
+++ src/main/java/com/codecool/kp/sffvectoradmin/controller/BookController.java
@@ -3,10 +3,7 @@ package com.codecool.kp.sffvectoradmin.controller;
 import com.codecool.kp.sffvectoradmin.model.book.BookList;
 import com.codecool.kp.sffvectoradmin.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
